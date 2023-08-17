61bcaaeddae6537dcacd5134983c5246209ac293
ZERO-JIFAR <juanignacioarnold@gmail.com>
2023-08-16 23:52:03 -0300

    Terminado

---
+++ ejer_comp.py
@@ -0,0 +1,71 @@
+#ejercicio 1
+numero1 = 3
+print(numero1)
+
+#ejercicio 2
+numero2 = 9.99
+print(numero2)
+
+#ejercicio 3
+suma = numero1 + numero2
+print (suma)
+
+#ejercicio 4
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+print (resta)
+print (multiplicacion)
+print (division)
+
+#ejercicio 5
+nombre = "Ignacio"
+print(nombre)
+
+#ejercicio 6
+precio = 3.90
+print(precio)
+
+#ejercicio 7
+descuento = 0.1
+print(descuento)
+
+#ejercicio 8
+precio_final = precio - (precio * descuento)
+print(precio_final)
+
+#ejercicio 9
+cadena = "Por el emperador"
+print(cadena)
+
+#ejercicio 10
+longitud = len (cadena)
+print(longitud)
+
+#ejercicio 11
+precio = 5.99
+precio2 = int (precio)
+print(precio2)
+
+#ejercicio 12
+nombre = "Ignacio"
+apellido = "Arnold"
+nombre_completo = nombre + " " + apellido
+print (nombre_completo)
+
+#ejercicio 13
+edad = 27 * 5 / 10
+print(edad)
+
+#ejercicio 14
+altura = 1.84 * 4 / 3
+print(altura)
+
+#ejercicio 15
+nombre_mayuscula = "IGNACIO ARNOLD"
+nombre_minuscula = nombre_mayuscula.swapcase() 
+print(nombre_minuscula)
+
+#ejercicio 16
+nombre_minuscula2 = nombre_mayuscula.capitalize ()
+print(nombre_minuscula2)
