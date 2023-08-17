37450aa1c84b421a739aa359a66e77813df5ad52
ZERO-JIFAR <juanignacioarnold@gmail.com>
2023-08-17 00:12:39 -0300

    Version final

--- ejercicios_complementarios.py
+++ ejercicios_complementarios.py
@@ -1,71 +1,71 @@
 #ejercicio 1
-numero1 = 3
-print(numero1)
+numero1 = 1
+print(" 1) Num1:",numero1)
 
 #ejercicio 2
-numero2 = 9.99
-print(numero2)
+numero2 = 2.1
+print(" 2) Num2:",numero2)
 
 #ejercicio 3
 suma = numero1 + numero2
-print (suma)
+print (" 3) Suma:",suma)
 
 #ejercicio 4
 resta = numero1 - numero2
 multiplicacion = numero1 * numero2
 division = numero1 / numero2
-print (resta)
-print (multiplicacion)
-print (division)
+print (" 4) Resta:",resta)
+print (" 4) Multiplicacion:",multiplicacion)
+print (" 4) Division:",division)
 
 #ejercicio 5
 nombre = "Ignacio"
-print(nombre)
+print(" 5) Nombre:",nombre)
 
 #ejercicio 6
-precio = 3.90
-print(precio)
+precio = 6.90
+print(" 6) Precio:",precio)
 
 #ejercicio 7
-descuento = 0.1
-print(descuento)
+descuento = 0.75
+print(" 7) Descuento:",descuento)
 
 #ejercicio 8
 precio_final = precio - (precio * descuento)
-print(precio_final)
+print(" 8) Precio final:",precio_final)
 
 #ejercicio 9
 cadena = "Por el emperador"
-print(cadena)
+print(" 9) Frace:",cadena)
 
 #ejercicio 10
 longitud = len (cadena)
-print(longitud)
+print("10) Largo:",longitud)
 
 #ejercicio 11
-precio = 5.99
+precio = 11.99
 precio2 = int (precio)
-print(precio2)
+print("11) Precio:",precio2)
 
 #ejercicio 12
 nombre = "Ignacio"
 apellido = "Arnold"
 nombre_completo = nombre + " " + apellido
-print (nombre_completo)
+print("12) Nombre completo:",nombre_completo)
 
 #ejercicio 13
 edad = 27 * 5 / 10
-print(edad)
+print("13) Edad:",edad)
 
 #ejercicio 14
 altura = 1.84 * 4 / 3
-print(altura)
+print("14) Altura:",altura)
 
 #ejercicio 15
 nombre_mayuscula = "IGNACIO ARNOLD"
 nombre_minuscula = nombre_mayuscula.swapcase() 
-print(nombre_minuscula)
+print("15) Nombre en minuscula:",nombre_minuscula)
 
 #ejercicio 16
 nombre_minuscula2 = nombre_mayuscula.capitalize ()
-print(nombre_minuscula2)
+print("16) nombre en minuscula",nombre_minuscula2)
