6ea5918080497c1e865715d45871cb4288432cc3
ZERO-JIFAR <juanignacioarnold@gmail.com>
2023-08-20 02:27:59 -0300

    trabajo parctico n1

---
+++ trabajo_practico_n1_parte_2.py
@@ -0,0 +1,44 @@
+#ejercicio 1
+
+#ejercicio 2
+
+#ejercicio 3
+
+#ejercicio 4
+
+#ejercicio 5
+
+#ejercicio 6
+
+#ejercicio 7
+
+#ejercicio 8
+
+#ejercicio 9
+
+#ejercicio 10
+
+#ejercicio 11
+
+#ejercicio 12
+
+#ejercicio 13
+
+#ejercicio 14
+
+#ejercicio 15
+
+#ejercicio 16
+
+#ejercicio 17
+
+#ejercicio 18
+cena = float(input("Ingrese el valor de la cena: "))
+total = cena + (cena * 0.62) + (cena * .1)
+print(total)
+
+#ejercicio 19
+
+#ejercicio 20
+
+#ejercicio 21
