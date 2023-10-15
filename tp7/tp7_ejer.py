2541b0337dacdf41e6108a91e882db57b95de1de
ZERO-JIFAR <juanignacioarnold@gmail.com>
2023-10-15 19:13:50 -0300

    tp7 terminado

--- tp7/tp7_ejer.py
+++ tp7/tp7_ejer.py
@@ -1,45 +1,85 @@
 import tp7_fun
 
-list_num=[]
-while True:
-    p=int(input("Ingrese un numero (0 para salir): "))
-    list_num.append(p)
-    if p==0:
-        break
-print (list_num)
+list_num=tp7_fun.list_assem_num()
 
-#1
+print("")
+
+print("Ejercicio 1.")
+print("Ordenamiento de burbuja (ascendente)")
 list_num1=list_num.copy()
 print(tp7_fun.ejer1(list_num1))
 
-#5
-list_num2=list_num.copy()
-print(tp7_fun.ejer5(list_num2))
+print("")
 
-#6
-output = [0 for i in range(len(list_num))]
-count = [0 for i in range(10)]
-for i in list_num:
-    count[i] += 1
-for i in range(1, 10):
-    count[i] += count[i-1]
-for i in range(len(list_num)):
-    output[count[list_num[i]]-1] = list_num[i]
-    count[list_num[i]] -= 1
-print(output)
+print("Ejercicio 2")
+print("Ordenamiento de selección")
+list_word=tp7_fun.list_assem_word()
+list_word2=list_word.copy()
+print("Palabras ordenadas alfabéticamente:", tp7_fun.selection_sort_words(list_word2))
 
+print("")
 
-def countSort(arr):
-    output = [0 for i in range(len(arr))]
-    count = [0 for i in range(10)]
-    for i in arr:
-        count[i] += 1
-    for i in range(1, 10):
-        count[i] += count[i-1]
-    for i in range(len(arr)):
-        output[count[arr[i]]-1] = arr[i]
-        count[arr[i]] -= 1
-    return output
-arr = [1, 4, 1, 2, 7, 5, 2]
-ans = countSort(arr)
-print(ans)
+print("Ejercicio 3.")
+books = [
+    {"titulo": "Yo Robot", "autor": "Isaac Asimov", "año": 1950},
+    {"titulo": "Dune", "autor": "Frank Herbert", "año": 1965},
+    {"titulo": "Veinte Mil Leguas de Viaje Submarino", "autor": "Julio Verne", "año": 1870},
+    {"titulo": "It", "autor": "Stephen King", "año": 1986}
+]
+parameter = input("Ingrese el parámetro de ordenamiento (titulo/autor/año): ").lower()
+if parameter not in ["titulo", "autor", "año"]:
+    print("Parámetro no válido. Debe ser 'titulo', 'autor' o 'año'.")
+else:
+    ordered_books = sorted(books, key=lambda x: x[parameter])
+    print("Libros ordenados por", parameter)
+    for book in ordered_books:
+        print(f"Título: {book['titulo']}, Autor: {book['autor']}, Año: {book['año']}")
+
+print("")
+
+print("Ejercicio 4")
+print("Ordenamiento de inserción")
+list_word4=list_word.copy()
+tp7_fun.insertionSort(list_word4)
+print("Palabras ordenadas en orden ascendente segun su longitud:",list_word4)
+
+print("")
+
+print("Ejercicio 5.")
+print("Ordenamiento de burbuja (descendente)")
+list_num5=list_num.copy()
+print(tp7_fun.ejer5(list_num5))
+
+print("")
+
+print("Ejercicio 6.")
+print("Ordenamiento por conteo")
+list_num6=list_num.copy()
+print(tp7_fun.ejer6(list_num6))
+
+print("")
+
+print("Ejercicio 7")
+words_and_numbers = []
+words =[]
+numbers = []
+while True:
+    list_input = input("Ingrese numeros y palabras (0 para salir): ").lower()
+    if list_input == '0':
+        break
+    else:
+        words_and_numbers.append(list_input)
+for element in words_and_numbers:
+    if element.isalpha():
+        words.append(element)
+    else:
+        numbers.append(int(element))
+words_and_numbers = tp7_fun.bubbleSort(numbers) + tp7_fun.insertionSort(words)
+print("Números ordenados de menor a mayor y cadenas ordenadas alfabéticamente: ", words_and_numbers)
+
+print("")
+
+print("Ejercicio 8")
+print("Ordenamiento Merge Sort")
+list_num8=list_num.copy()
+print(tp7_fun.merge_sort(list_num8))
--- tp7/tp7_fun.py
+++ tp7/tp7_fun.py
@@ -1,3 +1,29 @@
+
+#cargado de datos de la lista de numeros
+def list_assem_num():
+    list_num0=[]
+    while True:
+        p=int(input("Ingrese un numero (0 para salir): "))
+        list_num0.append(p)
+        if p==0:
+            break
+    print (list_num0)
+    return list_num0
+
+#cargado de datos de la lista de palabras
+def list_assem_word():
+    list_word0=[]
+    while True:
+        p=str(input("Ingrese una palabra (vacio para salir): "))
+        if p==" ":
+            break
+        elif p.isalpha():
+            list_word0.append(p)
+        else:
+            print("Ingrese una palabra.")
+    print (list_word0)
+    return list_word0
+
 #1
 def ejer1(list_num1):
     n = len(list_num1)
@@ -7,6 +33,32 @@ def ejer1(list_num1):
                     list_num1[j], list_num1[j+1] = list_num1[j+1], list_num1[j]
     return list_num1
 
+#2
+def selection_sort_words(list_word2):
+    len_word = len(list_word2)
+    for i in range(len_word - 1):
+        min_index = i
+        for j in range(i + 1, len_word):
+            if list_word2[j] < list_word2[min_index]:
+                min_index = j
+        list_word2[i], list_word2[min_index] = list_word2[min_index], list_word2[i]
+    return list_word2
+
+#3
+
+#4
+def insertionSort(li):
+    n = len(li)
+    if n <= 1:
+        return li
+    for i in range(1, n):
+        key = li[i]
+        j = i-1
+        while j >= 0 and len(key) < len(li[j]):
+            li[j+1] = li[j]
+            j -= 1
+        li[j+1] = key
+
 #5
 def ejer5(list_num2):
     n = len(list_num2)
@@ -15,3 +67,69 @@ def ejer5(list_num2):
                 if list_num2[j] < list_num2[j+1]:
                     list_num2[j], list_num2[j+1] = list_num2[j+1], list_num2[j]
     return list_num2
+
+#6
+def ejer6(list_num6):
+    output = [0 for i in range(len(list_num6))]
+    count = [0 for i in range(1000000)]
+    for i in list_num6:
+        count[i] += 1
+    for i in range(1, 1000000):
+        count[i] += count[i-1]
+    for i in range(len(list_num6)):
+        output[count[list_num6[i]]-1] = list_num6[i]
+        count[list_num6[i]] -= 1
+    return output
+
+#7
+def insertionSort(word_list):
+    if len(word_list) <= 1:
+        return word_list
+    else:    
+        for i in range(1, len(word_list)):
+            key = word_list[i]
+            j = i-1
+            while j >= 0 and key < word_list[j]:
+                word_list[j+1] =  word_list[j]
+                j -= 1
+            word_list[j+1] = key
+        return word_list
+
+def bubbleSort(number_list):
+    n = len(number_list)
+    swapped = False
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if number_list[j] > number_list[j + 1]:
+                swapped = True
+                number_list[j], number_list[j + 1] = number_list[j + 1], number_list[j]
+        if not swapped:
+            return number_list
+    return number_list
+
+#8
+def merge_sort(arr):
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+        merge_sort(left_half)
+        merge_sort(right_half)
+        i = j = k = 0
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[j]:
+                arr[k] = left_half[i]
+                i += 1
+            else:
+                arr[k] = right_half[j]
+                j += 1
+            k += 1
+        while i < len(left_half):
+            arr[k] = left_half[i]
+            i += 1
+            k += 1
+        while j < len(right_half):
+            arr[k] = right_half[j]
+            j += 1
+            k += 1
+    return arr
